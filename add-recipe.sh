79fa04aac548cde914335363393648e83c56beb3
sasheto <alexander@vassilevski.com>
2025-06-05 13:52:53 -0700

    fixing add recipe script

--- add-recipe.sh
+++ add-recipe.sh
@@ -12,13 +12,13 @@ ACCESS_TOKEN=`curl -s -X POST -H "Content-type: application/json" \
      http://localhost:3000/api/v1/register | jq .access_token | sed 's/"//g'`
 
 JOKE='{
-  "id": "40",
+  "id": 40,
   "title": "Sugar milk",
   "category": "drink",
   "ingredient_amount": [
     "100ml milk", "20g sugar"
   ],
-  "preparation": "put the sugar in the milk and stir",
+  "preparation": "put the sugar in the milk and stir"
 }'
 
 curl -X POST -H "Content-type: application/json"  \
